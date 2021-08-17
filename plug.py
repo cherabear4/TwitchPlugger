f45ef67136f7cb295fb22442fdeaca3f72fab983
piggydoe <67165527+piggydoe@users.noreply.github.com>
2021-08-17 13:00:29 -0700

    Fixed!

--- plug.py
+++ plug.py
@@ -4,18 +4,17 @@ import keyboard
 
 text_file = open("config.txt", "r")
 lines = text_file.readline()
-print(lines)
 text_file.close()
 
 
-def plug():
-    if keyboard.is_pressed('ctrl+shift'):
-        while True:
+def main():
+    while True:
+        if keyboard.is_pressed('-'):
             pyautogui.press('enter')
             time.sleep(.3)
-            pyautogui.write('go follow me on ' + 'twitch ' +
-                            '@' + lines)
+            pyautogui.write(lines)
             pyautogui.press('enter')
 
 
-plug()
+while True:
+    main()
